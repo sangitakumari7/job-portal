76472c7671ff4281c90fdf633174a89dd13af228
sangitakumari7 <sangitaroy4829@gmail.com>
2026-05-09 02:22:06 +0530

    fix: clearCookie options for cross-origin

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -28,11 +28,11 @@ export const signUp=async (req,res)=>{
 
        let token=await genToken(user._id)
        res.cookie("token",token,{
-        httpOnly:true,
-        maxAge:7*24*60*60*1000,
-        sameSite:"strict",
-        secure:process.env.NODE_ENVIRONMENT==="production"
-       })
+  httpOnly:true,
+  maxAge:7*24*60*60*1000,
+  sameSite:"none",
+  secure:true
+})
       return res.status(201).json(user)
 
     } catch (error) {
@@ -59,8 +59,8 @@ export const login=async (req,res)=>{
         res.cookie("token",token,{
          httpOnly:true,
          maxAge:7*24*60*60*1000,
-         sameSite:"strict",
-         secure:process.env.NODE_ENVIRONMENT==="production"
+         sameSite:"none",
+         secure:true
         })
        return res.status(200).json(user)
     } catch (error) {
@@ -71,7 +71,11 @@ export const login=async (req,res)=>{
 
 export const logOut=async (req,res)=>{
     try {
-        res.clearCookie("token")
+        res.clearCookie("token",{
+          httpOnly:true,
+          sameSite:"none",
+          secure:true
+        })
         return res.status(200).json({message:"log out successfully"})
     } catch (error) {
         console.log(error);
