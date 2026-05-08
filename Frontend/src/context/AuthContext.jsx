3174b42450c9e7e40d2a11801136e06ac6fe7f9a
sangitakumari7 <sangitaroy4829@gmail.com>
2026-05-09 01:22:47 +0530

    fix: use env variable for serverUrl

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,16 +1,18 @@
 import React, { createContext } from 'react'
-export const authDataContext=createContext()
-function AuthContext({children}) {
-const serverUrl="http://localhost:8000"
-    let value={
-        serverUrl
-    }
+
+export const authDataContext = createContext()
+
+function AuthContext({ children }) {
+  const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:8000"
+
+  let value = {
+    serverUrl
+  }
+
   return (
-    <div>
-     <authDataContext.Provider value={value}> 
-     {children}
-     </authDataContext.Provider> 
-    </div>
+    <authDataContext.Provider value={value}>
+      {children}
+    </authDataContext.Provider>
   )
 }
 
